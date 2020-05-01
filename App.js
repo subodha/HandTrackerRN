a36eb03a46969285e8a1f86b6dab46c72780dd8d
Subodha Pathiraja <subodha@smashydesign.com>
2020-05-01 18:03:03 +0530

    Initial image clasification test

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import * as tf from '@tensorflow/tfjs';
-import { fetch, decodeJpeg } from '@tensorflow/tfjs-react-native';
+import { fetch, decodeJpeg, cameraWithTensors } from '@tensorflow/tfjs-react-native';
+import { Camera } from 'expo-camera';
+
+const TensorCamera = cameraWithTensors(Camera);
+
 
 import * as mobilenet from '@tensorflow-models/mobilenet';
  
@@ -11,23 +15,42 @@ export default class App extends React.Component {
     this.state = {
       isTfReady: false,
     };
+
+    const image = require('./assets/cat.png');
   }
+
+  // handleCameraStream(images, updatePreview, gl) {
+  //   const loop = async () => {
+  //     const nextImageTensor = images.next().value
+
+  //     //
+  //     // do something with tensor here
+  //     //
+
+  //     // if autorender is false you need the following two lines.
+  //     // updatePreview();
+  //     // gl.endFrameEXP();
+
+  //     // requestAnimation(loop);
+  //   }
+  //   loop();
+  // }
 
  
   async componentDidMount() {
     // Wait for tf to be ready.
     await tf.ready();
     // Signal to the app that tensorflow.js can now be used.
-    // this.setState({
-    //   isTfReady: true,
-    // });
+    this.setState({
+      isTfReady: true,
+    });
 
     // Load mobilenet.
     const model = await mobilenet.load();
 
     // Get a reference to the bundled asset and convert it to a tensor
-    const image = require('./assets/cat.jpg');
-    const imageAssetPath = Image.resolveAssetSource(image);
+    
+    const imageAssetPath = Image.resolveAssetSource(require('./assets/cat.png'));
     const response = await fetch(imageAssetPath.uri, {}, { isBinary: true });
     const imageData = await response.arrayBuffer();
 
@@ -43,6 +66,23 @@ export default class App extends React.Component {
  
  
   render() {
+
+    // Currently expo does not support automatically determining the
+    // resolution of the camera texture used. So it must be determined
+    // empirically for the supported devices and preview size.
+
+    let textureDims;
+    if (Platform.OS === 'ios') {
+      textureDims = {
+        height: 200,
+        width: 300,
+      };
+    } else {
+      textureDims = {
+        height: 200,
+        width: 300,
+      };
+    }
     return (
       <View style={styles.container}>
         <Text>Hy Subo, start working on your app!</Text>
@@ -50,8 +90,8 @@ export default class App extends React.Component {
          {this.state.prediction && <Text>{this.state.prediction}</Text>} 
          {this.state.prediction && console.log(this.state.prediction)}
          <Image
-          style={{ width: 50, height: 50 }}
-          source={this.image}
+          style={{ width: 250, height: 200 }}
+          source={require('./assets/cat.png')}
         />
       </View>
     );
@@ -66,4 +106,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     color: '#fff',
   },
+  camera: {
+    width: 300,
+    height: 400,
+  }
 });
